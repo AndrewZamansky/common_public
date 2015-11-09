6197398fc7ee4377e66b6ebe3b2c4395b3ad454f
andrew <stimpy1@gmail.com>
2015-11-09 14:59:13 +0200

    improved git managment

--- c/build_tools/makefiles/common_init.mk
+++ c/build_tools/makefiles/common_init.mk
@@ -109,15 +109,21 @@ $(info scan for uconfig.mk done )
 
 include config.mk
 
-SHELL_OUTPUT := $(shell $(SHELL_GO_TO_COMMON_GIT_DIR) git rev-parse --abbrev-ref HEAD)
-ifneq ($(findstring $(PROJECT_NAME),$(SHELL_OUTPUT)),$(PROJECT_NAME)) 	 
-    DUMMY = $(shell $(SHELL_GO_TO_COMMON_GIT_DIR) git checkout $(PROJECT_NAME))
+CURR_GIT_BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
+ifeq ($(findstring master,$(CURR_GIT_BRANCH)),master) 	 
+    $(error  branch names must be of type $(PROJECT_NAME) or $(PROJECT_NAME)_<branch_name>)
 endif
 
-SHELL_OUTPUT := $(shell git rev-parse --abbrev-ref HEAD)
-ifneq ($(findstring $(PROJECT_NAME),$(SHELL_OUTPUT)),$(PROJECT_NAME)) 	 
-    DUMMY = $(shell git checkout $(PROJECT_NAME))
+SHELL_OUTPUT := $(shell $(SHELL_GO_TO_COMMON_GIT_DIR) git rev-parse --abbrev-ref HEAD)
+ifneq ($(sort $(filter $(CURR_GIT_BRANCH),$(SHELL_OUTPUT))),$(CURR_GIT_BRANCH)) 	 
+    SHELL_OUTPUT := $(shell $(SHELL_GO_TO_COMMON_GIT_DIR) git checkout $(CURR_GIT_BRANCH) 2>&1)
+    ERROR_MESSAGE :=did not match any file
+    ifeq ($(findstring $(ERROR_MESSAGE),$(SHELL_OUTPUT)),$(ERROR_MESSAGE)) 	 
+        $(error  branch $(CURR_GIT_BRANCH) not found in common git . create it)
+    endif
 endif
+
+
 
 OUTPUT_APP_NAME := out
 ifdef CONFIG_OUTPUT_NAME
