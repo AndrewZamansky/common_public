106ded127b4bba52e6c1d38fab2e93962c4f9f4b
gituser <stimpy1@gmail.com>
2017-07-16 11:14:55 +0300

    fix esp8266 postbiuld makefile

--- c/hw_drivers/arch/TENSILICA/soc/esp8266/Makefile.postbuild.mk
+++ c/hw_drivers/arch/TENSILICA/soc/esp8266/Makefile.postbuild.mk
@@ -15,18 +15,23 @@ endif
 include $(MAKEFILES_ROOT_DIR)/_include_functions/tool_existence_check.mk
 ####### end of tool existence test #####
 
+BIN_GEN_ORIG_SCRIPT :=$(SDK_ESP8266_PATH)\tools\gen_appbin.py
+FLASH_BIN_FILE :=$(OUT_DIR)/eagle.app.flash.bin
 
 COMPILER_PATH :=$(GCC_ROOT_DIR)/bin
 ifeq ($(findstring WINDOWS,$(COMPILER_HOST_OS)),WINDOWS)
     COMPILER_PATH := $(subst /,\,$(COMPILER_PATH))
     SDK_ESP8266_PATH := $(subst /,\,$(SDK_ESP8266_PATH))
+    BIN_GEN_ORIG_SCRIPT := $(subst /,\,$(BIN_GEN_ORIG_SCRIPT))
+    FLASH_BIN_FILE := $(subst /,\,$(FLASH_BIN_FILE))
     CD := cd /D 
+    RM := del /Q
 endif
 
 BUILD_FLASH_IMAGE :=set PATH=%PATH%;$(COMPILER_PATH)&
 BUILD_FLASH_IMAGE += set COMPILE=gcc&
-BUILD_FLASH_IMAGE += $(CP) $(SDK_ESP8266_PATH)\tools\gen_appbin.py $(OUT_DIR)&
-BUILD_FLASH_IMAGE += $(CD) $(OUT_DIR) &
+BUILD_FLASH_IMAGE += $(CP) $(BIN_GEN_ORIG_SCRIPT) $(OUT_DIR)&
+BUILD_FLASH_IMAGE += $(CD) $(OUT_DIR)&
 ifdef CONFIG_SDK_NONOS
     BUILD_FLASH_IMAGE += gen_appbin.py $(LINKER_OUTPUT) 0 0 0 0 0
 else ifdef CONFIG_SDK_RTOS
@@ -42,6 +47,7 @@ build_outputs:
 	$(FULL_GCC_PREFIX)objcopy --only-section .data -O binary $(LINKER_OUTPUT) $(OUT_DIR)/eagle.app.v6.data.bin
 	$(FULL_GCC_PREFIX)objcopy --only-section .rodata -O binary $(LINKER_OUTPUT) $(OUT_DIR)/eagle.app.v6.rodata.bin
 	$(FULL_GCC_PREFIX)objcopy --only-section .irom0.text -O binary $(LINKER_OUTPUT) $(OUT_DIR)/eagle.app.v6.irom0text.bin
+	-$(RM) $(FLASH_BIN_FILE)
 	$(BUILD_FLASH_IMAGE)
 ifeq ($(findstring WINDOWS,$(COMPILER_HOST_OS)),WINDOWS)
 	@echo ---- INFO : rm error message CAN BE IGNORED IN WINDOWS
