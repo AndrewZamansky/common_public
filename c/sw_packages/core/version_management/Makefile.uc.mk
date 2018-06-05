b6db1e3100833321e08c001371e382b465ab04a0
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2018-06-05 08:44:49 -0700

    force version management to be compiled always

--- c/sw_packages/core/version_management/Makefile.uc.mk
+++ c/sw_packages/core/version_management/Makefile.uc.mk
@@ -10,6 +10,15 @@ INCLUDE_THIS_COMPONENT := $(CONFIG_INCLUDE_VERSION_MENAGMENT)
 
 #ASMFLAGS =  
 
+# force this component to be compiled always to get updated version
+CURRENT_COMPILATION_DIR_NAME := $(notdir $(abspath .))
+CURR_OBJ_DIR := $(OBJ_DIR)/$(CURRENT_COMPILATION_DIR_NAME)
+ifeq ($(findstring WINDOWS,$(COMPILER_HOST_OS)),WINDOWS)
+    CURR_OBJ_DIR :=$(subst /,\,$(CURR_OBJ_DIR))
+endif
+SHELL_OUT :=$(shell $(RM) $(CURR_OBJ_DIR) 2>&1)
+
+
 DEFINES += VERSION_STR="$(FULL_VERSION_STR)"
 
 
