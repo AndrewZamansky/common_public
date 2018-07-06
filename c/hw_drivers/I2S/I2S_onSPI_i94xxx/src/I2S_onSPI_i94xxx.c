10c23fe82a4244e77c77131784a8eeb990bdd5b5
nwong1 <nwong1@nuvoton.com>
2018-07-05 18:47:16 -0700

    Add pins to device tree for I2S over SPI. Removed schmitt trigger for I2S module.

--- c/hw_drivers/I2S/I2S_onSPI_i94xxx/src/I2S_onSPI_i94xxx.c
+++ c/hw_drivers/I2S/I2S_onSPI_i94xxx/src/I2S_onSPI_i94xxx.c
@@ -23,45 +23,13 @@
 
 /********  defines *********************/
 
+//Pin definitions not included in the I94xxx BSP v3.03
 #define SYS_GPC_MFPL_PC0MFP_SPI1_MOSI           (0x03UL<<SYS_GPC_MFPL_PC0MFP_Pos) /* SPI1 MOSI (Master Out, Slave In) pin; or I2S1 data output pin. */
 #define SYS_GPC_MFPL_PC1MFP_SPI1_MISO           (0x03UL<<SYS_GPC_MFPL_PC1MFP_Pos) /* SPI1 MISO (Master In, Slave Out) pin; or I2S1 data input pin. */
 #define SYS_GPC_MFPL_PC2MFP_SPI1_CLK            (0x03UL<<SYS_GPC_MFPL_PC2MFP_Pos) /* SPI1 Serial Clock pin; or I2S1 bit clock pin. */
 #define SYS_GPC_MFPL_PC3MFP_SPI1_SS             (0x03UL<<SYS_GPC_MFPL_PC3MFP_Pos) /* SPI1 slave select pin; or I2S1 left right channel clock pin. */
 #define SYS_GPC_MFPL_PC4MFP_SPI1_I2SMCLK        (0x03UL<<SYS_GPC_MFPL_PC4MFP_Pos) /* SPI1 I2S master clock output pin. */
 
-
-#define SPI1_PD_MSK     SYS_GPD_MFPL_PD2MFP_Msk  | \
-                        SYS_GPD_MFPL_PD3MFP_Msk  | \
-                        SYS_GPD_MFPL_PD4MFP_Msk  | \
-                        SYS_GPD_MFPL_PD5MFP_Msk  | \
-                        SYS_GPD_MFPL_PD6MFP_Msk
-
-#define SPI1_PD_POS     SYS_GPD_MFPL_PD2MFP_SPI1_MOSI     | \
-                        SYS_GPD_MFPL_PD3MFP_SPI1_MISO     | \
-                        SYS_GPD_MFPL_PD4MFP_SPI1_CLK      | \
-                        SYS_GPD_MFPL_PD5MFP_SPI1_SS       | \
-                        SYS_GPD_MFPL_PD6MFP_SPI1_I2SMCLK
-
-
-#define SPI1_PC_MSK     SYS_GPC_MFPL_PC0MFP_Msk  | \
-                        SYS_GPC_MFPL_PC1MFP_Msk  | \
-                        SYS_GPC_MFPL_PC2MFP_Msk  | \
-                        SYS_GPC_MFPL_PC3MFP_Msk  | \
-                        SYS_GPC_MFPL_PC4MFP_Msk
-
-#define SPI1_PC_POS     SYS_GPC_MFPL_PC0MFP_SPI1_MOSI    | \
-						SYS_GPC_MFPL_PC1MFP_SPI1_MISO    | \
-						SYS_GPC_MFPL_PC2MFP_SPI1_CLK     | \
-						SYS_GPC_MFPL_PC3MFP_SPI1_SS      | \
-						SYS_GPC_MFPL_PC4MFP_SPI1_I2SMCLK
-
-#define SPI2_PA_MSK
-
-#define SPI2_PA_POS
-
-#define SPI2_PC_MSK
-
-#define SPI2_PC_POS
 
 /********  types  *********************/
 
@@ -73,9 +41,6 @@
 /* ---------------- Exported variables --------------------------*/
 
 /* ---------------- Main variables ------------------------------*/
-#define TEST_COUNT	128
-
-static volatile int g_u32DataCount = 0;
 
 /* ---------------- Interrupt Routine ---------------------------*/
 
@@ -84,8 +49,6 @@ static volatile int g_u32DataCount = 0;
  * TODO: Make a data transfer interface.
  */
 
-static uint32_t u32RxValue[2];
-
 uint8_t I2S_onSPI_i94xxx_callback(struct dev_desc_t *adev ,
 		uint8_t aCallback_num , void * aCallback_param1,
 		void * aCallback_param2)
@@ -93,6 +56,7 @@ uint8_t I2S_onSPI_i94xxx_callback(struct dev_desc_t *adev ,
 	struct I2S_onSPI_i94xxx_cfg_t *cfg_hndl;
 	SPI_T	*I2S_SPI_module;
 
+	static uint32_t u32RxValue[2];
 	uint32_t audio_format, data_width;
 
 	uint8_t i;
@@ -218,41 +182,100 @@ uint8_t I2S_onSPI_i94xxx_callback(struct dev_desc_t *adev ,
  * 		Output = None
  */
 
-static void configure_i2s_spi_pinout(uint8_t spi_bus)
+static void configure_i2s_spi_pinout(struct dev_desc_t *adev)
 {
-	switch(spi_bus)
+	struct I2S_onSPI_i94xxx_cfg_t *cfg_hndl;
+	cfg_hndl = DEV_GET_CONFIG_DATA_POINTER(adev);
+
+	switch(cfg_hndl->base_address)
 	{
-	case SPI1_PD :
-		//Reset and configure pins
-		SYS->GPD_MFPL &= ~SPI1_PD_MSK;
-		SYS->GPD_MFPL |= SPI1_PD_POS;
+	case I2S_onSPI_I94XXX_API_BASE_ADDRESS_SPI1 :
+		if(cfg_hndl->BCLK_pin == I2S_onSPI_I94XXX_API_BCLK_PIN_D4)
+		{
+			SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD4MFP_Msk);
+			SYS->GPD_MFPL |= SYS_GPD_MFPL_PD4MFP_SPI1_CLK;
 
-		//Enable schmitt triggering for signal integrity when using both
-		//  input and output. (Problem with i94124 chip)
-		PD->SMTEN = GPIO_SMTEN_SMTEN2_Msk |
-					GPIO_SMTEN_SMTEN3_Msk |
-					GPIO_SMTEN_SMTEN4_Msk |
-					GPIO_SMTEN_SMTEN5_Msk |
-					GPIO_SMTEN_SMTEN6_Msk;
+			PD->SMTEN = GPIO_SMTEN_SMTEN6_Msk;
+		}
+
+		if(cfg_hndl->LRCLK_pin == I2S_onSPI_I94XXX_API_LRCLK_PIN_D5)
+		{
+			SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD5MFP_Msk);
+			SYS->GPD_MFPL |= SYS_GPD_MFPL_PD5MFP_SPI1_SS;
+
+			PD->SMTEN = GPIO_SMTEN_SMTEN3_Msk;
+		}
+
+		if(cfg_hndl->DI_pin == I2S_onSPI_I94XXX_API_DI_PIN_D3)
+		{
+			SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD3MFP_Msk);
+			SYS->GPD_MFPL |= SYS_GPD_MFPL_PD3MFP_SPI1_MISO;
+
+			PD->SMTEN = GPIO_SMTEN_SMTEN4_Msk;
+		}
+
+		if(cfg_hndl->DO_pin == I2S_onSPI_I94XXX_API_DO_PIN_D2)
+		{
+			SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD2MFP_Msk);
+			SYS->GPD_MFPL |= SYS_GPD_MFPL_PD2MFP_SPI1_MOSI;
+
+			PD->SMTEN = GPIO_SMTEN_SMTEN5_Msk;
+		}
+
+		if(cfg_hndl->MCLK_pin == I2S_onSPI_I94XXX_API_MCLK_PIN_D6)
+		{
+			SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD6MFP_Msk);
+			SYS->GPD_MFPL |= SYS_GPD_MFPL_PD6MFP_SPI1_I2SMCLK;
+
+			PD->SMTEN = GPIO_SMTEN_SMTEN2_Msk;
+		}
+
+		if(cfg_hndl->BCLK_pin == I2S_onSPI_I94XXX_API_BCLK_PIN_C2)
+		{
+			SYS->GPC_MFPL &= ~(SYS_GPC_MFPL_PC2MFP_Msk);
+			SYS->GPC_MFPL |= SYS_GPC_MFPL_PC2MFP_SPI1_CLK;
+
+			PC->SMTEN = GPIO_SMTEN_SMTEN2_Msk;
+		}
+
+		if(cfg_hndl->LRCLK_pin == I2S_onSPI_I94XXX_API_LRCLK_PIN_C3)
+		{
+			SYS->GPC_MFPL &= ~(SYS_GPC_MFPL_PC3MFP_Msk);
+			SYS->GPC_MFPL |= SYS_GPC_MFPL_PC3MFP_SPI1_SS;
+
+			PC->SMTEN = GPIO_SMTEN_SMTEN3_Msk;
+		}
+
+		if(cfg_hndl->DI_pin == I2S_onSPI_I94XXX_API_DI_PIN_C1)
+		{
+			SYS->GPC_MFPL &= ~(SYS_GPC_MFPL_PC1MFP_Msk);
+			SYS->GPC_MFPL |= SYS_GPC_MFPL_PC1MFP_SPI1_MISO;
+
+			PC->SMTEN = GPIO_SMTEN_SMTEN1_Msk;
+		}
+
+		if(cfg_hndl->DO_pin == I2S_onSPI_I94XXX_API_DO_PIN_C0)
+		{
+			SYS->GPC_MFPL &= ~(SYS_GPC_MFPL_PC0MFP_Msk);
+			SYS->GPC_MFPL |= SYS_GPC_MFPL_PC0MFP_SPI1_MOSI;
+
+			PC->SMTEN = GPIO_SMTEN_SMTEN0_Msk;
+		}
+
+		if(cfg_hndl->MCLK_pin == I2S_onSPI_I94XXX_API_MCLK_PIN_C4)
+		{
+			SYS->GPC_MFPL &= ~(SYS_GPC_MFPL_PC4MFP_Msk);
+			SYS->GPC_MFPL |= SYS_GPC_MFPL_PC4MFP_SPI1_I2SMCLK;
+
+			PC->SMTEN = GPIO_SMTEN_SMTEN4_Msk;
+		}
+		break;
+	case I2S_onSPI_I94XXX_API_BASE_ADDRESS_SPI2 :
+		CRITICAL_ERROR("SPI2 pinouts not defined yet");
 		break;
 
-	case SPI1_PC :
-		//Reset and configure pins
-		SYS->GPC_MFPL &= ~SPI1_PC_MSK;
-		SYS->GPC_MFPL |= SPI1_PC_POS;
-
-		//Enable schmitt triggering for signal integrity when using both
-		//  input and output. (Problem with i94124 chip)
-		PC->SMTEN = GPIO_SMTEN_SMTEN0_Msk |
-					GPIO_SMTEN_SMTEN1_Msk |
-					GPIO_SMTEN_SMTEN2_Msk |
-					GPIO_SMTEN_SMTEN3_Msk |
-					GPIO_SMTEN_SMTEN4_Msk;
-		break;
-	case SPI2_PA :
-		break;
-	case SPI2_PC :
-		break;
+	default:
+		CRITICAL_ERROR("SPI base address needs to be defined");
 	}
 }
 
@@ -275,7 +298,17 @@ uint8_t I2S_onSPI_i94xxx_ioctl( struct dev_desc_t *adev,
 	cfg_hndl = DEV_GET_CONFIG_DATA_POINTER(adev);
 	I2S_SPI_module = (SPI_T*)cfg_hndl->base_address;
 	src_clock = cfg_hndl->src_clock;
+	uint32_t clk_mode;
+	uint32_t sample_rate;
+	uint32_t data_width;
+	uint32_t audio_format;
+	uint32_t txrx_format;
 
+	clk_mode = cfg_hndl->clk_mode;
+	sample_rate = cfg_hndl->sample_rate;
+	data_width = cfg_hndl->data_width;
+	audio_format = cfg_hndl->audio_format;
+	txrx_format = cfg_hndl->txrx_format;
 
 	switch(aIoctl_num)
 	{
@@ -283,19 +316,18 @@ uint8_t I2S_onSPI_i94xxx_ioctl( struct dev_desc_t *adev,
 		if ((SPI_T*)SPI1_BASE == I2S_SPI_module) clk_dev = i94xxx_spi1clk_clk_dev;
 		else if ((SPI_T*)SPI2_BASE == I2S_SPI_module) clk_dev = i94xxx_spi2clk_clk_dev;
 
-		configure_i2s_spi_pinout(cfg_hndl->spi_bus);
+		configure_i2s_spi_pinout(adev);
 
 		DEV_IOCTL_1_PARAMS(clk_dev,	CLK_IOCTL_SET_PARENT, src_clock);
 		DEV_IOCTL_0_PARAMS(clk_dev, CLK_IOCTL_ENABLE);
 
-		SPI_I2SOpen(I2S_SPI_module,
-					cfg_hndl->clk_mode,
-					cfg_hndl->sample_rate,
-					cfg_hndl->data_width,
-					cfg_hndl->audio_format,
-					cfg_hndl->txrx_format);
+		SPI_I2SOpen(I2S_SPI_module, clk_mode, sample_rate, data_width,
+											audio_format, txrx_format);
 
-		//Interrupt based on receive buffer.
+		if(NULL != cfg_hndl->MCLK_pin)
+		{
+			SPI_I2SEnableMCLK(I2S_SPI_module, sample_rate * 256 );
+		}
 
 		SPI_I2S_SET_RXTH(SPI1, SPI_I2S_FIFO_RX_LEVEL_4);
 		SPI_I2S_SET_TXTH(SPI1, SPI_I2S_FIFO_TX_LEVEL_1);
@@ -303,6 +335,12 @@ uint8_t I2S_onSPI_i94xxx_ioctl( struct dev_desc_t *adev,
 		SPI_I2S_RST_TX_FIFO(I2S_SPI_module);
 		SPI_I2S_RST_RX_FIFO(I2S_SPI_module);
 
+		/*
+		 * Commented lines activate the interrupt which follows the callback
+		 *   function. Currently using the DMA so interrupt is not needed.
+		 *   Please refer to device tree for current set up to comment or
+		 *   uncomment these lines.
+		 */
 //		SPI_I2SEnableInt(I2S_SPI_module, SPI_I2S_RXTH_INT_MASK);
 //
 //		if (SPI1_BASE == I2S_SPI_module) i2s_spi_irq = SPI1_IRQn;
