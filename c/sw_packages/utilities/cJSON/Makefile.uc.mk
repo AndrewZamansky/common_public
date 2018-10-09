20627fc69946e9dc641d670bb23603575ac7fbff
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2018-10-09 08:32:04 -0700

    upmerge cJSON

--- c/sw_packages/utilities/cJSON/Makefile.uc.mk
+++ c/sw_packages/utilities/cJSON/Makefile.uc.mk
@@ -7,14 +7,21 @@ ifeq ($(strip $(CONFIG_INCLUDE_CJSON)),y)
     ifeq ("$(wildcard $(CJSON_PATH))","")
         $(info   )
         $(info --- cJSON path $(CJSON_PATH) dont exists )
-        $(info --- get repo from andew zamansky or from https://github.com/kbranigan/cJSON.git  )
-        $(info --- make sure that .git directory is located in $(CJSON_PATH)/  after unpacking   )
+        $(info --- get repo from andew zamansky or from https://github.com/DaveGamble/cJSON.git)
+        $(info --- make sure that .git directory is located in $(CJSON_PATH)/  after unpacking)
         $(error )
     endif
+
+    # test if current commit and branch of cJSON git is
+    # the same as required by application
+    CURR_GIT_REPO_DIR :=$(CJSON_PATH)
+    CURR_GIT_COMMIT_HASH_VARIABLE :=CONFIG_CJSON_GIT_COMMIT_HASH
+    include $(MAKEFILES_ROOT_DIR)/_include_functions/git_prebuild_repo_check.mk
 
     ifeq ("","$(filter $(CJSON_PATH),$(EXTERNAL_SRC_DIRS))")
         EXTERNAL_SRC_DIRS := $(EXTERNAL_SRC_DIRS) $(CJSON_PATH)
     endif
+
 
     #CURR_CJSON_COMPONENT_LOCATION := $(patsubst %/Makefile.uc.mk,%,$(realpath $(filter %cJSON/Makefile.uc.mk,$(MAKEFILE_LIST))))
     DUMMY := $(call ADD_TO_GLOBAL_INCLUDE_PATH , $(CJSON_PATH))
