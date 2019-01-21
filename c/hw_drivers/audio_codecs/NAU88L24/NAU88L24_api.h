c39afa199905da771b837baecef3ceeb18ac98c6
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2019-01-21 15:10:15 -0800

    add NAU88L24 driver

---
+++ c/hw_drivers/audio_codecs/NAU88L24/NAU88L24_api.h
@@ -0,0 +1,28 @@
+
+#ifndef _NAU88L24_API_H
+#define _NAU88L24_API_H
+
+
+
+/*****************  defines  **************/
+
+
+/**********  define API  types ************/
+
+
+
+/**********  define API  functions  ************/
+
+#define  NAU88L24_Input_type_none   1
+#define  NAU88L24_Input_type_stereo_aux  2
+#define  NAU88L24_Input_type_MicM   3
+#define  NAU88L24_Input_type_MicP   4
+#define  NAU88L24_Input_type_DMIC   5
+
+
+enum NAU88L24_Output_type {
+	NAU88L24_Output_type_none,
+	NAU88L24_Output_type_headset
+};
+
+#endif
