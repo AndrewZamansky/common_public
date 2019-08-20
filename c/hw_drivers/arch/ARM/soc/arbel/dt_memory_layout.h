0d45e8e8dc1d1f29ab4f4e7772677118c3eb2e31
Ohad Russo <ohad.russo@nuvoton.com>
2019-08-21 01:00:26 +0300

    set default DDR for arbel to be 32MB

--- c/hw_drivers/arch/ARM/soc/arbel/dt_memory_layout.h
+++ c/hw_drivers/arch/ARM/soc/arbel/dt_memory_layout.h
@@ -20,7 +20,7 @@
 #define USE_EXTERNAL_DDR
 #define DT_EXTERNAL_DDR_NAME          external_ddr
 #define DT_EXTERNAL_DDR_BASE_ADDR     0x8000
-#define DT_EXTERNAL_DDR_MEMORY_SIZE   0x40000
+#define DT_EXTERNAL_DDR_MEMORY_SIZE   0x2000000//32MB
 
 #include "set_memory_layout.h"
 
