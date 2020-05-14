553f2c36adf6fd6a616ad37b0f1beedd13d53a57
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2020-05-14 13:37:33 -0700

    improve readability of heartbeat

--- c/sw_packages/core/heartbeat/src/heartbeat.c
+++ c/sw_packages/core/heartbeat/src/heartbeat.c
@@ -8,30 +8,26 @@
 #include "_project_typedefs.h"
 #include "_project_defines.h"
 
-
 #include "heartbeat_api.h"
-
 #include "heartbeat.h"
-
 #include "hw_timer_api.h"
-
 #include "irq_api.h"
-
 //#include "dev_management_api.h"
 #include "os_wrapper.h"
 
 #define MODULE_NAME             heartbeat
 
-static volatile uint32_t cpuUsageCounter;
-static volatile uint32_t cpu1secUsageCounter = 0;
-static volatile uint32_t restart_counter = 1;
-static volatile uint32_t callibrationDone = 0;
-//uint32_t idleCpuUsageCounter;
+#define SKIP_MEASURES	3
+
+static size_t empty_cpu_usage_counter_per_mSec = 1;
+static volatile uint32_t cpu_usage_counter;
+static volatile uint32_t cpu_usage_counter_1sec = 0;
+static volatile uint32_t timer_tick_counter = 1;
+static volatile uint32_t callibration_done = 0;
+//uint32_t idlecpu_usage_counter;
 static uint32_t cpu_usage_measure_mPercents = 0;
 static int16_t one_sec_countdown = 1000;
 
-
-#define SKIP_MEASURES	3
 
 /* Function:        heartbeat_timer_callback
  *
@@ -39,8 +35,90 @@ static int16_t one_sec_countdown = 1000;
  */
 static void heartbeat_timer_callback()
 {
-	restart_counter++;
+	timer_tick_counter++;
 }
+
+
+static void cpu_usage_calibration(struct heartbeat_instance_t *handle)
+{
+	cpu_usage_counter = 0;
+	DEV_IOCTL_1_PARAMS(handle->callibration_timer,
+			IOCTL_TIMER_CALLBACK_SET, heartbeat_timer_callback);
+	DEV_IOCTL_0_PARAMS(handle->callibration_timer, IOCTL_DEVICE_START );
+
+	irq_unblock_all();
+	while (1)
+	{
+		cpu_usage_counter++;
+		if (0 == timer_tick_counter)
+		{
+			// if timer didn't tick, then continue to count CPU usage
+			continue;
+		}
+		if(SKIP_MEASURES == callibration_done)
+		{
+			break; // exit while(1) loop with current cpu usage counter
+		}
+		callibration_done++;
+		cpu_usage_counter = 0;
+		timer_tick_counter = 0;
+	}
+	irq_block_all();
+	DEV_IOCTL_0_PARAMS(handle->callibration_timer, IOCTL_TIMER_STOP );
+	empty_cpu_usage_counter_per_mSec = cpu_usage_counter;
+}
+
+
+static void calculate_cpu_usage_per_sec(struct heartbeat_instance_t *handle)
+{
+	uint32_t cpu_free_mPercents;
+	uint32_t one_tick_cpu_usage;
+	struct dev_desc_t * heartbeat_callback_dev;
+
+	heartbeat_callback_dev = handle->heartbeat_callback_dev;
+
+	while (1)
+	{
+		cpu_usage_counter++;
+		if (0 == timer_tick_counter)
+		{
+			// if timer didn't tick, then continue to count CPU usage
+			continue;
+		}
+
+		// if for some reason current counter is bigger then counter on
+		// empty CPU, then it looks that empty CPU was running slower during
+		// calibration. In this case update "empty_cpu_counter"
+		one_tick_cpu_usage = cpu_usage_counter / timer_tick_counter;
+		if ( one_tick_cpu_usage > empty_cpu_usage_counter_per_mSec)
+		{
+			empty_cpu_usage_counter_per_mSec = one_tick_cpu_usage;
+			one_sec_countdown = 1000;
+			cpu_usage_counter_1sec = 0;
+		}
+
+		one_sec_countdown -= timer_tick_counter;
+		cpu_usage_counter_1sec += cpu_usage_counter;
+		if (0 >= one_sec_countdown)
+		{
+			cpu_free_mPercents = (cpu_usage_counter_1sec * 100) /
+										empty_cpu_usage_counter_per_mSec;
+			cpu_usage_measure_mPercents = 100000 - cpu_free_mPercents;
+			if (NULL != heartbeat_callback_dev)
+			{
+				DEV_CALLBACK_0_PARAMS(heartbeat_callback_dev,
+										HEARTBEAT_API_HEARTBEAT_TICK);
+			}
+			cpu_usage_counter_1sec = 0;
+			one_sec_countdown = 1000;
+
+			os_low_stack_trap(32);
+		}
+		cpu_usage_counter = 0;
+		timer_tick_counter = 0;
+	}
+}
+
 
 /**
  * heartbeat_ioctl()
@@ -51,88 +129,20 @@ static uint8_t heartbeat_ioctl( struct dev_desc_t *adev,
 		const uint8_t aIoctl_num, void * aIoctl_param1, void * aIoctl_param2)
 {
 	struct heartbeat_instance_t *handle;
-	static size_t ticks_per_mSec = 1;
 
 	handle = DEV_GET_CONFIG_DATA_POINTER(MODULE_NAME, adev);
 	switch (aIoctl_num)
 	{
 	case IOCTL_DEVICE_START :
-
-		DEV_IOCTL_1_PARAMS(handle->callibration_timer ,
-				IOCTL_TIMER_CALLBACK_SET, heartbeat_timer_callback);
-		DEV_IOCTL_0_PARAMS(handle->callibration_timer, IOCTL_DEVICE_START );
-
-		irq_unblock_all();
-		while (1)
-		{
-			if ( restart_counter )
-			{
-				if(SKIP_MEASURES == callibrationDone)
-				{
-					break;
-				}
-				callibrationDone++;
-				cpuUsageCounter = 0;
-				restart_counter = 0;
-			}
-			cpuUsageCounter++;
-		}
-		irq_block_all();
-
-		ticks_per_mSec = cpuUsageCounter;
-		//idleCpuUsageCounter = cpuUsageCounter * 1000;
-		cpuUsageCounter=0;
-
-		callibrationDone++;
-
-
-		DEV_IOCTL_0_PARAMS(handle->callibration_timer , IOCTL_TIMER_STOP );
-
+		cpu_usage_calibration(handle);
 		break;
 
-
 	case HEARTBEAT_API_EACH_1mS_CALL:
-		restart_counter++;
+		timer_tick_counter++;
 		break;
 
 	case HEARTBEAT_API_CALL_FROM_IDLE_TASK :
-		while (1)
-		{
-			if (restart_counter )
-			{
-				uint16_t tmp_restart_counter = restart_counter;
-
-				one_sec_countdown -= tmp_restart_counter;
-				cpu1secUsageCounter += cpuUsageCounter;
-				if (0 >= one_sec_countdown)
-				{
-					struct dev_desc_t * heartbeat_callback_dev;
-					cpu_usage_measure_mPercents = 100000 -
-							( (cpu1secUsageCounter * 100) / ticks_per_mSec );
-					heartbeat_callback_dev = handle->heartbeat_callback_dev;
-					if (NULL != heartbeat_callback_dev)
-					{
-						DEV_CALLBACK_0_PARAMS( heartbeat_callback_dev,
-												HEARTBEAT_API_HEARTBEAT_TICK );
-					}
-					cpu1secUsageCounter = 0;
-					one_sec_countdown = 1000;
-
-					os_low_stack_trap(32);
-				}
-				cpuUsageCounter = cpuUsageCounter / restart_counter;
-				if ( cpuUsageCounter > ticks_per_mSec)
-				{
-					ticks_per_mSec = cpuUsageCounter;
-					one_sec_countdown = 1000;
-					cpu1secUsageCounter = 0;
-				}
-				restart_counter = 0;
-				cpuUsageCounter = 0;
-			}
-			cpuUsageCounter++;
-
-		}
+		calculate_cpu_usage_per_sec(handle);
 		break;
 
 	case HEARTBEAT_API_GET_CPU_USAGE:
@@ -142,7 +152,7 @@ static uint8_t heartbeat_ioctl( struct dev_desc_t *adev,
 	case HEARTBEAT_API_BUSY_WAIT_mS:
 		{
 			size_t ticks_to_wait = (size_t)aIoctl_param1;
-			ticks_to_wait = ticks_to_wait * ticks_per_mSec;
+			ticks_to_wait = ticks_to_wait * empty_cpu_usage_counter_per_mSec;
 			while (ticks_to_wait--);
 		}
 		break;
