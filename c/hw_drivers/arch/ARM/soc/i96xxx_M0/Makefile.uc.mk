b9300016c4773a50f3e7ac099f0bb070334b1b7e
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2020-12-17 17:32:05 -0800

    add git to I96 BSP

--- c/hw_drivers/arch/ARM/soc/i96xxx_M0/Makefile.uc.mk
+++ c/hw_drivers/arch/ARM/soc/i96xxx_M0/Makefile.uc.mk
@@ -1,17 +1,20 @@
 
 ifeq ($(sort $(CONFIG_I96XXX_M0)),y)
 
-    I96XXX_BSP_VERSION :=$(patsubst "%",%,$(CONFIG_I96XXX_BSP_VERSION))
-    _BSP_DIR_NAME =BSP_I96XXX_v$(I96XXX_BSP_VERSION)
-    BSP_I96XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/$(_BSP_DIR_NAME)
+    BSP_I96XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/BSP_I96XXX
     ifeq ("$(wildcard $(BSP_I96XXX_PATH))","")
         $(info !--- I96XXX BSP path $(BSP_I96XXX_PATH) does not exist)
-        $(info !--- download I96XXX BSP version $(I96XXX_BSP_VERSION) and)
-        $(info !--- unpack it to $(BSP_I96XXX_PATH))
+        $(info !--- get I96XXX BSP git and unpack it to $(BSP_I96XXX_PATH))
         $(info !--- make sure Library and SampleCode folders are)
         $(info !--- located in $(BSP_I96XXX_PATH)/  after unpacking)
         $(error )
     endif
+
+    # test if current commit and branch of uboot git is the same
+    # as required by application
+    CURR_GIT_REPO_DIR :=$(BSP_I96XXX_PATH)
+    CURR_GIT_COMMIT_HASH_VARIABLE :=CONFIG_BSP_I96XXX_GIT_COMMIT_HASH
+    include $(MAKEFILES_ROOT_DIR)/_include_functions/git_prebuild_repo_check.mk
 
     ifeq ("","$(filter $(BSP_I96XXX_PATH),$(EXTERNAL_SRC_DIRS))")
         EXTERNAL_SRC_DIRS := $(EXTERNAL_SRC_DIRS) $(BSP_I96XXX_PATH)
@@ -23,9 +26,9 @@ ifeq ($(sort $(CONFIG_I96XXX_M0)),y)
     DUMMY := $(call ADD_TO_GLOBAL_INCLUDE_PATH , $(INC_PATH))
     INC_PATH := $(BSP_I96XXX_PATH)/Library/Device/Nuvoton/Include
     DUMMY := $(call ADD_TO_GLOBAL_INCLUDE_PATH , $(INC_PATH))
-	I96XXX_SRC_DIR := $(BSP_I96XXX_PATH)/Library/StdDriver/src
-	INCLUDE_THIS_COMPONENT := y
-	
+    I96XXX_SRC_DIR := $(BSP_I96XXX_PATH)/Library/StdDriver/src
+    INCLUDE_THIS_COMPONENT := y
+
 endif
 
 SRC += board_init.c
--- c/hw_drivers/arch/XTENSA/soc/i96xxx_HiFi3/Makefile.uc.mk
+++ c/hw_drivers/arch/XTENSA/soc/i96xxx_HiFi3/Makefile.uc.mk
@@ -1,16 +1,21 @@
 ifeq ($(sort $(CONFIG_I96XXX_HIFI3)),y)
     INCLUDE_THIS_COMPONENT := y
-    I96XXX_BSP_VERSION :=$(patsubst "%",%,$(CONFIG_I96XXX_BSP_VERSION_HIFI3))
-    _BSP_DIR_NAME =BSP_I96XXX_v$(I96XXX_BSP_VERSION)
-    BSP_I96XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/$(_BSP_DIR_NAME)
+
+    BSP_I96XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/BSP_I96XXX
     ifeq ("$(wildcard $(BSP_I96XXX_PATH))","")
         $(info !--- I96XXX BSP path $(BSP_I96XXX_PATH) does not exist)
-        $(info !--- download I96XXX BSP version $(I96XXX_BSP_VERSION) and)
-        $(info !--- unpack it to $(BSP_I96XXX_PATH))
-        $(info !--- make sure Library and SampleCode folders is)
+        $(info !--- get I96XXX BSP git and unpack it to $(BSP_I96XXX_PATH))
+        $(info !--- make sure Library and SampleCode folders are)
         $(info !--- located in $(BSP_I96XXX_PATH)/  after unpacking)
         $(error )
     endif
+
+    # test if current commit and branch of uboot git is the same
+    # as required by application
+    CURR_GIT_REPO_DIR :=$(BSP_I96XXX_PATH)
+    CURR_GIT_COMMIT_HASH_VARIABLE :=CONFIG_BSP_I96XXX_GIT_COMMIT_HASH
+    include $(MAKEFILES_ROOT_DIR)/_include_functions/git_prebuild_repo_check.mk
+
 
     ifeq ("","$(filter $(BSP_I96XXX_PATH),$(EXTERNAL_SRC_DIRS))")
         EXTERNAL_SRC_DIRS := $(EXTERNAL_SRC_DIRS) $(BSP_I96XXX_PATH)
