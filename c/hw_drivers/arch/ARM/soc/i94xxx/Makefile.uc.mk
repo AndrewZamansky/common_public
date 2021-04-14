dca2d9cef8e97dbcf19713ec2663d3d0c4b91f28
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2021-04-14 16:03:30 -0700

    add git control to i94xxx BSP

--- c/hw_drivers/arch/ARM/soc/i94xxx/Makefile.uc.mk
+++ c/hw_drivers/arch/ARM/soc/i94xxx/Makefile.uc.mk
@@ -1,17 +1,21 @@
 
 ifeq ($(sort $(CONFIG_I94XXX)),y)
 
-    I94XXX_BSP_VERSION :=$(patsubst "%",%,$(CONFIG_I94XXX_BSP_VERSION))
-    _BSP_DIR_NAME =BSP_I94XXX_v$(I94XXX_BSP_VERSION)
-    BSP_I94XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/$(_BSP_DIR_NAME)
+    BSP_I94XXX_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/BSP_I94XXX
     ifeq ("$(wildcard $(BSP_I94XXX_PATH))","")
         $(info !--- I94XXX BSP path $(BSP_I94XXX_PATH) does not exist)
-        $(info !--- download I94XXX BSP version $(I94XXX_BSP_VERSION) and)
-        $(info !--- unpack it to $(BSP_I94XXX_PATH).  Make sure Library and)
-        $(info !--- SampleCode folders is located)
-        $(info !--- in $(BSP_I94XXX_PATH)/  after unpacking)
+        $(info !--- get I94XXX BSP git and unpack it to $(BSP_I94XXX_PATH))
+        $(info !--- make sure Library and SampleCode folders are)
+        $(info !--- located in $(BSP_I94XXX_PATH)/  after unpacking)
         $(error )
     endif
+
+    # test if current commit and branch of uboot git is the same
+    # as required by application
+    CURR_GIT_REPO_DIR :=$(BSP_I94XXX_PATH)
+    CURR_GIT_COMMIT_HASH_VARIABLE :=CONFIG_BSP_I94XXX_GIT_COMMIT_HASH
+    include $(MAKEFILES_ROOT_DIR)/_include_functions/git_prebuild_repo_check.mk
+
 
     ifeq ("","$(filter $(BSP_I94XXX_PATH),$(EXTERNAL_SRC_DIRS))")
         EXTERNAL_SRC_DIRS := $(EXTERNAL_SRC_DIRS) $(BSP_I94XXX_PATH)
