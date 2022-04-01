a79458a41264740a5d1aa0ef1855e1829206578e
UL00 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2022-04-01 10:49:15 -0700

    add interrupt base data transfer on I2S I94xxx

--- c/hw_drivers/I2S/I2S_onSPI_i94xxx/src/I2S_onSPI_i94xxx.h
+++ c/hw_drivers/I2S/I2S_onSPI_i94xxx/src/I2S_onSPI_i94xxx.h
@@ -11,7 +11,10 @@
 #include "dev_management_api.h"
 
 struct I2S_onSPI_i94xxx_runtime_t {
-	uint8_t   init_done;
+	uint8_t                         init_done;
+	i2s_on_spi_interrupt_handler_t  int_handler;
+	uint8_t                         *in_buff;
+	uint32_t                        overflow_counter;
 };
 SET_RUNTIME_DATA_TYPE(I2S_onSPI_i94xxx, struct I2S_onSPI_i94xxx_runtime_t);
 
--- c/hw_drivers/arch/ARM/soc/i94xxx/dt_interrupts_num.h
+++ c/hw_drivers/arch/ARM/soc/i94xxx/dt_interrupts_num.h
@@ -6,6 +6,6 @@
 #ifndef dt_interrupts_num_h
 #define dt_interrupts_num_h
 
-#define NUMBER_OF_NVIC_EXTERNAL_INTERRUPTS   68
+#define NUMBER_OF_NVIC_EXTERNAL_INTERRUPTS   69
 
-#endif /* */
+#endif
