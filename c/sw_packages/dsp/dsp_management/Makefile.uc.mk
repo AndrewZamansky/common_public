d48f3f3db6a35b00908f58af8330e6afa5dfb9a7
UL50 Andrew Zamansky <AZAMANSKY@nuvoton.com>
2023-02-02 15:06:25 -0800

    add git synchronization to libfixmath

--- c/sw_packages/dsp/dsp_management/Makefile.uc.mk
+++ c/sw_packages/dsp/dsp_management/Makefile.uc.mk
@@ -16,6 +16,24 @@ SRC = dsp_management.cpp
 VPATH = src
 
 ifdef CONFIG_DSP_REAL_NUMBER_FORMAT_FIXED_POINT
+
+    LIBFIXMATH_GIT_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/libfixmath
+    ifeq ("$(wildcard $(LIBFIXMATH_GIT_PATH))","")
+        $(info  )
+        $(info !--- libfixmath path $(LIBFIXMATH_GIT_PATH) does not exists)
+        $(info !--- download/clone libfixmath repository to)
+        $(info !--- $(LIBFIXMATH_GIT_PATH) and make sure that .git directory is)
+        $(info !--- located in $($(LIBFIXMATH_GIT_PATH))/  after unpacking)
+        $(info  )
+        $(error )
+    endif
+
+    # test if current commit and branch of uboot git is the same
+    # as required by application
+    CURR_GIT_REPO_DIR :=$(LIBFIXMATH_GIT_PATH)
+    CURR_GIT_COMMIT_HASH_VARIABLE :=CONFIG_LIBFIXMATH_GIT_COMMIT_HASH
+    include $(MAKEFILES_ROOT_DIR)/_include_functions/git_prebuild_repo_check.mk
+
     ifdef CONFIG_FIXMATH_NO_CACHE
         DUMMY := $(call ADD_TO_GLOBAL_DEFINES, FIXMATH_NO_CACHE )
     endif
@@ -25,7 +43,8 @@ ifdef CONFIG_DSP_REAL_NUMBER_FORMAT_FIXED_POINT
     ifdef CONFIG_FIXMATH_NO_ROUNDING
         DUMMY := $(call ADD_TO_GLOBAL_DEFINES, FIXMATH_NO_ROUNDING )
     endif
-    LIBFIXMATH_PATH :=$(EXTERNAL_SOURCE_ROOT_DIR)/libfixmath/trunk/libfixmath
+
+    LIBFIXMATH_PATH :=$(LIBFIXMATH_GIT_PATH)/libfixmath
     DUMMY := $(call ADD_TO_GLOBAL_INCLUDE_PATH, $(LIBFIXMATH_PATH))
     SRC += fix16.c
     SRC += fix16_exp.c
