f1c8ecaee8d723d94b71deb3a1810b9b9d7fd736
Andrew Zamansky (NTC) <azamansk@ilab519.nuvoton.com>
2025-03-13 18:04:10 +0200

    improve tavor selection

--- c/hw_drivers/arch/ARM/soc/tavor/dt_memory_layout.h
+++ c/hw_drivers/arch/ARM/soc/tavor/dt_memory_layout.h
@@ -10,19 +10,26 @@
 
 #define USE_INTERNAL_SRAM
 #define DT_INTERNAL_SRAM_NAME          internal_sram
-#define DT_INTERNAL_SRAM_BASE_ADDR     0xFFFB0000
-#if defined(CONFIG_ARBEL_256K_SRAM)
+#if defined(CONFIG_TAVOR_SRAM_BASE_0x0)
+	#define DT_INTERNAL_SRAM_BASE_ADDR     0x00000000
+#else
+	#define DT_INTERNAL_SRAM_BASE_ADDR     0x1000000
+#endif
+#if defined(CONFIG_TAVOR_64K_SRAM)
+	#define DT_INTERNAL_SRAM_MEMORY_SIZE   0x10000
+#elif defined(CONFIG_TAVOR_256K_SRAM)
 	#define DT_INTERNAL_SRAM_MEMORY_SIZE   0x40000
 #else
 	#error "Tavor flavor not defined"
 #endif
 
+
 #define USE_EXTERNAL_DDR
 #define DT_EXTERNAL_DDR_NAME          external_ddr
 #define DT_EXTERNAL_DDR_BASE_ADDR     0x80000000
-#if defined(TAVOR_2MB_DDR)
+#if defined(CONFIG_TAVOR_2MB_DDR)
 	#define DT_EXTERNAL_DDR_MEMORY_SIZE   0x200000//2MB
-#elif defined(TAVOR_32MB_DDR)
+#elif defined(CONFIG_TAVOR_32MB_DDR)
 	#define DT_EXTERNAL_DDR_MEMORY_SIZE   0x2000000//32MB
 #else
 	#error "Tavor flavor not defined"
